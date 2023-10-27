5e4dc3634b2e9b78a64f3acb444f05bc255a5fef
L3CTOR-ED <romohov.1918@yandex.ru>
2023-10-27 09:43:45 +0400

    First-com

---
+++ Python/education/class_object.py
@@ -0,0 +1,8 @@
+class cat:
+    def __init__(self, size, color, cat_type):
+        self.size = size
+        self.color = color
+        self.cat_type = cat_type
+
+cat_1 = cat(size='small')
+print(cat_1.size)
